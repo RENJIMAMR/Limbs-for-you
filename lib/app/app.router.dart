4f0bfc045cde95504d8e178a5d4082ceb67cf6c7
RENJIMAMR <renjima24@gmail.com>
2024-10-03 11:09:47 +0530

    hh

---
+++ lib/app/app.router.dart
@@ -0,0 +1,561 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedNavigatorGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:carify_clone_two/ui/screens/bottom_nav_bar/bottom_nav_bar_view.dart'
+    as _i4;
+import 'package:carify_clone_two/ui/screens/company_list/company_list_view.dart'
+    as _i6;
+import 'package:carify_clone_two/ui/screens/favorite_screen/favorite_screen_view.dart'
+    as _i9;
+import 'package:carify_clone_two/ui/screens/home/home_view.dart' as _i5;
+import 'package:carify_clone_two/ui/screens/item_detailing_screen/item_detailing_screen_view.dart'
+    as _i10;
+import 'package:carify_clone_two/ui/screens/prosthesis_detailing_screen/prosthesis_detailing_screen_view.dart'
+    as _i8;
+import 'package:carify_clone_two/ui/screens/search/search_view.dart' as _i7;
+import 'package:carify_clone_two/ui/screens/splash/splash_view.dart' as _i2;
+import 'package:carify_clone_two/ui/screens/welcome/welcome_view.dart' as _i3;
+import 'package:flutter/material.dart' as _i11;
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart' as _i1;
+import 'package:stacked_services/stacked_services.dart' as _i12;
+
+class Routes {
+  static const splashView = '/';
+
+  static const welcomeView = '/welcome-view';
+
+  static const bottomNavBarView = '/bottom-nav-bar-view';
+
+  static const homeView = '/home-view';
+
+  static const companyListView = '/company-list-view';
+
+  static const searchView = '/search-view';
+
+  static const prosthesisDetailingScreenView =
+      '/prosthesis-detailing-screen-view';
+
+  static const favoriteScreenView = '/favorite-screen-view';
+
+  static const itemDetailingScreenView = '/item-detailing-screen-view';
+
+  static const all = <String>{
+    splashView,
+    welcomeView,
+    bottomNavBarView,
+    homeView,
+    companyListView,
+    searchView,
+    prosthesisDetailingScreenView,
+    favoriteScreenView,
+    itemDetailingScreenView,
+  };
+}
+
+class StackedRouter extends _i1.RouterBase {
+  final _routes = <_i1.RouteDef>[
+    _i1.RouteDef(
+      Routes.splashView,
+      page: _i2.SplashView,
+    ),
+    _i1.RouteDef(
+      Routes.welcomeView,
+      page: _i3.WelcomeView,
+    ),
+    _i1.RouteDef(
+      Routes.bottomNavBarView,
+      page: _i4.BottomNavBarView,
+    ),
+    _i1.RouteDef(
+      Routes.homeView,
+      page: _i5.HomeView,
+    ),
+    _i1.RouteDef(
+      Routes.companyListView,
+      page: _i6.CompanyListView,
+    ),
+    _i1.RouteDef(
+      Routes.searchView,
+      page: _i7.SearchView,
+    ),
+    _i1.RouteDef(
+      Routes.prosthesisDetailingScreenView,
+      page: _i8.ProsthesisDetailingScreenView,
+    ),
+    _i1.RouteDef(
+      Routes.favoriteScreenView,
+      page: _i9.FavoriteScreenView,
+    ),
+    _i1.RouteDef(
+      Routes.itemDetailingScreenView,
+      page: _i10.ItemDetailingScreenView,
+    ),
+  ];
+
+  final _pagesMap = <Type, _i1.StackedRouteFactory>{
+    _i2.SplashView: (data) {
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i2.SplashView(),
+        settings: data,
+      );
+    },
+    _i3.WelcomeView: (data) {
+      final args = data.getArgs<WelcomeViewArguments>(
+        orElse: () => const WelcomeViewArguments(),
+      );
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => _i3.WelcomeView(key: args.key),
+        settings: data,
+      );
+    },
+    _i4.BottomNavBarView: (data) {
+      final args = data.getArgs<BottomNavBarViewArguments>(
+        orElse: () => const BottomNavBarViewArguments(),
+      );
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => _i4.BottomNavBarView(key: args.key),
+        settings: data,
+      );
+    },
+    _i5.HomeView: (data) {
+      final args = data.getArgs<HomeViewArguments>(
+        orElse: () => const HomeViewArguments(),
+      );
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => _i5.HomeView(key: args.key),
+        settings: data,
+      );
+    },
+    _i6.CompanyListView: (data) {
+      final args = data.getArgs<CompanyListViewArguments>(
+        orElse: () => const CompanyListViewArguments(),
+      );
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => _i6.CompanyListView(key: args.key),
+        settings: data,
+      );
+    },
+    _i7.SearchView: (data) {
+      final args = data.getArgs<SearchViewArguments>(
+        orElse: () => const SearchViewArguments(),
+      );
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => _i7.SearchView(key: args.key),
+        settings: data,
+      );
+    },
+    _i8.ProsthesisDetailingScreenView: (data) {
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i8.ProsthesisDetailingScreenView(),
+        settings: data,
+      );
+    },
+    _i9.FavoriteScreenView: (data) {
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i9.FavoriteScreenView(),
+        settings: data,
+      );
+    },
+    _i10.ItemDetailingScreenView: (data) {
+      return _i11.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i10.ItemDetailingScreenView(),
+        settings: data,
+      );
+    },
+  };
+
+  @override
+  List<_i1.RouteDef> get routes => _routes;
+
+  @override
+  Map<Type, _i1.StackedRouteFactory> get pagesMap => _pagesMap;
+}
+
+class WelcomeViewArguments {
+  const WelcomeViewArguments({this.key});
+
+  final _i11.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant WelcomeViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class BottomNavBarViewArguments {
+  const BottomNavBarViewArguments({this.key});
+
+  final _i11.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant BottomNavBarViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class HomeViewArguments {
+  const HomeViewArguments({this.key});
+
+  final _i11.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant HomeViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class CompanyListViewArguments {
+  const CompanyListViewArguments({this.key});
+
+  final _i11.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant CompanyListViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+class SearchViewArguments {
+  const SearchViewArguments({this.key});
+
+  final _i11.Key? key;
+
+  @override
+  String toString() {
+    return '{"key": "$key"}';
+  }
+
+  @override
+  bool operator ==(covariant SearchViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode;
+  }
+}
+
+extension NavigatorStateExtension on _i12.NavigationService {
+  Future<dynamic> navigateToSplashView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.splashView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToWelcomeView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.welcomeView,
+        arguments: WelcomeViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToBottomNavBarView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.bottomNavBarView,
+        arguments: BottomNavBarViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToHomeView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.homeView,
+        arguments: HomeViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToCompanyListView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.companyListView,
+        arguments: CompanyListViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToSearchView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo<dynamic>(Routes.searchView,
+        arguments: SearchViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToProsthesisDetailingScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.prosthesisDetailingScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToFavoriteScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.favoriteScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToItemDetailingScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.itemDetailingScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithSplashView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.splashView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithWelcomeView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.welcomeView,
+        arguments: WelcomeViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithBottomNavBarView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.bottomNavBarView,
+        arguments: BottomNavBarViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithHomeView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.homeView,
+        arguments: HomeViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithCompanyListView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.companyListView,
+        arguments: CompanyListViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithSearchView({
+    _i11.Key? key,
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return replaceWith<dynamic>(Routes.searchView,
+        arguments: SearchViewArguments(key: key),
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithProsthesisDetailingScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.prosthesisDetailingScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithFavoriteScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.favoriteScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithItemDetailingScreenView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.itemDetailingScreenView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+}
